6f7822d1b27f1340f362e0efe9601fde7ab1dd45
Davi Queiroz <daviqueiroz26@proton.me>
2026-03-30 14:10:39 -0300

    add car attributes

--- Racing Simulator.py
+++ Racing Simulator.py
@@ -1,1 +1,9 @@
 class Car:
+
+    def __init__(self, make, model, year):
+
+        self.make = make
+        self.model = model
+        self.year = year
+        self.fuel = 100
+        self.performance = 50
