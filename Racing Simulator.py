164f5cc1e9cfc52de1cfcbe57e5b77275b416a56
Davi Queiroz <daviqueiroz26@proton.me>
2026-03-30 14:28:33 -0300

    add race method

--- Racing Simulator.py
+++ Racing Simulator.py
@@ -16,3 +16,24 @@ class Car:
         print("============= YOUR 🏎️ CAR ===============")
         print(f"{self.make} {self.model} {self.year}"
               f"fuel: {self.fuel} Car Performance: {self.performance}")
+
+    def race(self):
+        if self.fuel >= 20:
+            print("\n================ RACE 🏎️ =================")
+            print(f"\nYou take your {self.model} out for a race..")
+            self.fuel -= 20
+            time.sleep(1.5)
+            winner = random.randint(1, 2)
+            if winner == 1:
+                print("\nYou won the race!")
+                self.credits += 10000
+                time.sleep(1)
+                print("+10,000 Credits! 💰")
+            elif winner == 2:
+                print("\nYou lost the race..")
+                self.credits -= 2000
+                time.sleep(1)
+                print("-2000 Credits! 🙉")
+        else:
+            print("\nYour outta fuel champ.")
+            time.sleep(1.5)
